fe9577ca9a95bcef2fc02ed2b3a8c6469de91859
Nagaraj Kuntoji <kn733478@gmail.com>
2025-11-04 09:05:39 +0530

    Set fixed values for principal, rate, and time

--- simple.py
+++ simple.py
@@ -1,5 +1,5 @@
-p=float(input("Enter the principal amount: "))
-r=float(input("Enter the rate of interest: "))
-t=float(input("Enter the time in years: ")) 
+p=500
+r=5
+t=1
 si=(p*r*t)/100
 print("The simple interest is:",si)
