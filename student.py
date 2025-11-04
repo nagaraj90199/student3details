fdd3511589705641a415fe518d54a2c66ce610c1
Nagaraj Kuntoji <kn733478@gmail.com>
2025-11-04 10:54:59 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,7 +1,7 @@
 import sys
 
-name=sys.argv[1]
-roll_no=sys.argv[2]
+name = sys.argv[0]
+roll_no = sys.argv[1]
 
-print("Student Name:",name)
-print("Student Roll no:",roll_no)
+print("Student Name:", name)
+print("Student Roll no:", roll_no)
